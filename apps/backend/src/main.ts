995e4467d8c180d33f3b9f210cb928834acb0ba3
Nick Aliferov <x8core@gmail.com>
2024-07-05 18:32:43 +0300

    add swagger draft

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,7 @@ import * as path from 'path';
 import { Request, Response } from 'express';
 import * as express from 'express';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestApplication>(AppModule);
@@ -17,6 +18,16 @@ async function bootstrap() {
   //app.useGlobalPipes(new NormalizeQueryParamsPipe());
 
   app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'dist')));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 
   app.use('*', (req: Request, res: Response) => {
